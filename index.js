23da59181b5b8907e5e9210b6040dd5af7f42a99
JojoSnow <milzwow94@gmail.com>
2022-01-25 12:47:13 +0200

    osa3c done

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const requestLogger = (request, response, next) => {
 app.use(requestLogger)
 
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -32,9 +32,11 @@ app.post('/api/notes', (request, response) => {
     date: new Date(),
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note
+    .save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => response.json(savedAndFormattedNote))
+    .catch(error => next(error))
 })
 
 app.get('/api/notes', (request, response) => {
@@ -89,6 +91,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
