0e7feaacbaf2b87261fa1d4376c5f71122557ede
JojoSnow <milzwow94@gmail.com>
2022-01-28 11:26:27 +0200

    osa3d done

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	'env': {
+		'node': true,
+		'commonjs': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		'eqeqeq': 'error',
+		'no-trailing-spaces': 'error',
+		'arrow-spacing': [
+			'error', { 'before': true, 'after': true}
+		],
+		'no-console': 0,
+	}
+}
